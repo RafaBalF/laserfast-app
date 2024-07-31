a84b736409f90216e3e30f9dc8ca3d56e49026b7
arthur <87774958+Arthur-T-Tavares@users.noreply.github.com>
2024-07-31 10:07:17 -0300

    componentes da tela de historico

--- lib/app/modules/historico/historico_page.dart
+++ lib/app/modules/historico/historico_page.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:intl/intl.dart';
 import 'package:laserfast_app/app/models/session.model.dart';
 import 'package:laserfast_app/app/modules/historico/historico_store.dart';
+import 'package:laserfast_app/app/shared/colors.dart';
+import 'package:laserfast_app/app/shared/text_styles.dart';
+import 'package:laserfast_app/app/shared/text_widget.dart';
 import 'package:laserfast_app/app/shared/widgets/divider_widget.dart';
 import 'package:laserfast_app/app/shared/widgets/shimmer_widget.dart';
 import 'package:laserfast_app/app/shared/widgets/simple_scaffold_widget.dart';
@@ -21,6 +25,15 @@ class HistoricoPageState extends State<HistoricoPage> {
 
   final double _height = 100.h;
   final double _width = 100.w;
+
+  final DateFormat sessionDateFormatter = DateFormat("dd/MM/yyyy 'as' HH:mm");
+
+  final realiza = const Color(0XFF65A491);
+  final falta = const Color(0xFFCB91D1);
+  final agendada = const Color(0XFFDDD2D2);
+  final andamento = const Color(0XFFE8C691);
+  final confirmada = const Color(0XFF817FF0);
+  final padrao = const Color(0XFF2B2B2B);
 
   @override
   void initState() {
@@ -87,8 +100,123 @@ class HistoricoPageState extends State<HistoricoPage> {
 
   Widget _sessionCard(SessionModel session) {
     return Padding(
-      padding: const EdgeInsets.all(24.0),
-      child: Text('${session.status}'),
+      padding: EdgeInsets.symmetric(vertical: 2.h),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              SizedBox(
+                width: 10.w,
+                child: Container(
+                  width: 30,
+                  height: 30,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: _getColor(session.statusCode!),
+                  ),
+                  child: const SizedBox(),
+                ),
+              ),
+              SizedBox(width: 2.5.w),
+              Flexible(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        textWidget(
+                          '${session.status}',
+                          style: label(),
+                        ),
+                        textWidget(
+                          sessionDateFormatter.format(session.date!),
+                          style: small(),
+                        ),
+                      ],
+                    ),
+                    DividerWidget(height: 0.5.h),
+                    textWidget(
+                      '${session.areas}',
+                      style: small(),
+                    ),
+                    textWidget(
+                      'Aplicador(a): ${session.applicator}',
+                      style: small(),
+                    ),
+                    DividerWidget(height: 0.5.h),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            width: 100.w,
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
+              _getStatusWidget(session.statusCode!),
+            ]),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Color _getColor(int statusCode) {
+    switch (statusCode) {
+      case 0:
+        return realiza;
+      case 1:
+        return falta;
+      case 2:
+        return agendada;
+      case 3:
+        return andamento;
+      case 4:
+        return confirmada;
+      default:
+        return padrao;
+    }
+  }
+
+  Widget _getStatusWidget(int statusCode) {
+    switch (statusCode) {
+      case 0:
+        return _cardBtn(realiza, 'CHECK-IN', () {});
+      case 1:
+        return const SizedBox();
+      case 2:
+        return GestureDetector(
+          onTap: () {},
+          child: Icon(
+            size: 4.h,
+            Icons.zoom_in,
+            color: black,
+          ),
+        );
+      case 3:
+        return const SizedBox();
+      case 4:
+        return _cardBtn(confirmada, 'CHECK-OUT', () {});
+      default:
+        return _cardBtn(realiza, 'CHECK-IN', () {});
+    }
+  }
+
+  Widget _cardBtn(Color color, String label, void Function()? onTap) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height: 2.5.h,
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(50),
+        ),
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 2.5.w),
+          child: textWidget(label, color: white),
+        ),
+      ),
     );
   }
 
