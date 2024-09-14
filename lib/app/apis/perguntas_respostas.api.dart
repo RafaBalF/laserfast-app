2cea6fc7f60e61569316e584815a5bed312aec8c
Arthur <arttedesqui@gmail.com>
2024-09-14 08:15:40 -0300

    adicionando TODO lembrando q o retorno dos endpoints estão errados

--- lib/app/apis/perguntas_respostas.api.dart
+++ lib/app/apis/perguntas_respostas.api.dart
@@ -19,6 +19,8 @@ class PerguntasRespostasApi extends BaseApi {
 
       var result = (await (await dio).get(url)).data;
 
+      // TODO: ESPERAR O MALUCO ARRUMAR O RETORNA DA API PRA TESTAR ESSA TELA
+
       b = BaseModel.fromJson(
         result,
         tipo: PerguntasRespostasModel(),
